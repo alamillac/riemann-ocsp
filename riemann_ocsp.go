c706faf88eb0816836752d27aca4679434a3ee4b
Andres <andres@lamillac.com>
2017-07-03 17:24:36 +0200

    Se adiciona metrica y estado numerico

--- riemann_ocsp.go
+++ riemann_ocsp.go
@@ -10,17 +10,22 @@ import (
 
 func main() {
 
-  if len(os.Args) != 8 {
-    fmt.Printf("usage: %s <tag> <host> <service> <state> <description> <ttl> <server:port>\n\n", os.Args[0])
+  if len(os.Args) != 9 {
+    fmt.Printf("usage: %s <tag> <host> <service> <state> <metric> <description> <ttl> <server:port>\n\n", os.Args[0])
     panic("wrong number of arguments")
   }
 
-  c, err := raidman.Dial("tcp", os.Args[7])
+  c, err := raidman.Dial("tcp", os.Args[8])
   if err != nil {
     panic(err)
   }
 
-  f, err := strconv.ParseFloat(os.Args[6], 32)
+  f, err := strconv.ParseFloat(os.Args[7], 32)
+  if err != nil {
+    panic(err)
+  }
+
+  m, err := strconv.ParseFloat(os.Args[5], 32)
   if err != nil {
     panic(err)
   }
@@ -35,13 +40,27 @@ func main() {
     default : state = strings.ToLower(os.Args[4])
   }
 
+  var num_state string
+
+  switch state {
+    case "ok": num_state = "0"
+    case "warning": num_state = "1"
+    case "critical": num_state = "2"
+    case "unknown": num_state = "3"
+    default : num_state = "3"
+  }
+
+  var service = os.Args[1] +string('.')+ strings.Replace(strings.ToLower(os.Args[3]), " ", "_", -1)
+
   var event = &raidman.Event{
     Host:         os.Args[2],
-    Service:      os.Args[3],
+    Service:      service,
     State:        state,
-    Description:  os.Args[5],
+    Metric:     float32(m),
+    Description:  os.Args[6],
     Ttl:          float32(f),
     Tags:         []string{os.Args[1]},
+    Attributes: map[string]string{"num_state": num_state},
   }
 
   err = c.Send(event)
