ee27f3b41c2e233278e8e3deeef935b15f10e934
alamilla <alamilla@unnax.com>
2018-11-05 13:31:25 +0100

    Cleaning

--- riemann_ocsp.go
+++ riemann_ocsp.go
@@ -11,11 +11,11 @@ import (
 func main() {
 
   if len(os.Args) != 9 {
-    fmt.Printf("usage: %s <tag> <host> <service> <state> <metric> <description> <ttl> <server:port>\n\n", os.Args[0])
+    fmt.Printf("usage: %s <server:port> <host> <service> <state> <metric> <description> <ttl>\n\n", os.Args[0])
     panic("wrong number of arguments")
   }
 
-  c, err := raidman.Dial("tcp", os.Args[8])
+  c, err := raidman.Dial("tcp", os.Args[1])
   if err != nil {
     panic(err)
   }
@@ -30,27 +30,9 @@ func main() {
     panic(err)
   }
 
-  var state string
+  var state string = strings.ToLower(os.Args[4])
 
-  switch os.Args[4] {
-    case "0": state = "ok"
-    case "1": state = "warning"
-    case "2": state = "critical"
-    case "3": state = "unknown"
-    default : state = strings.ToLower(os.Args[4])
-  }
-
-  var state_code string
-
-  switch state {
-    case "ok": state_code = "0"
-    case "warning": state_code = "1"
-    case "critical": state_code = "2"
-    case "unknown": state_code = "3"
-    default : state_code = "3"
-  }
-
-  var service = os.Args[1] +string('.')+ strings.Replace(strings.ToLower(os.Args[3]), " ", "_", -1)
+  var service = strings.Replace(strings.ToLower(os.Args[3]), " ", "_", -1)
 
   var event = &raidman.Event{
     Host:         os.Args[2],
@@ -59,8 +41,6 @@ func main() {
     Metric:     float32(m),
     Description:  os.Args[6],
     Ttl:          float32(f),
-    Tags:         []string{os.Args[1]},
-    Attributes: map[string]string{"state_code": state_code},
   }
 
   err = c.Send(event)
